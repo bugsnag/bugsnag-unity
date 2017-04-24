b3a1e07181cafe4af83211fa1fefbce06873c453
Dave Perryman <dave@bugsnag.com>
2017-04-24 15:09:17 +0100

    [ENG-857] Support programmatically setting API key (#41)
    * updated bugsnag.cs to have a static way of setting the API key
    * Changed to log an error message if the API key could not be found
    * Add factory method to create Bugsnag instance with API key already set

--- example/Assets/Standard Assets/Bugsnag/Bugsnag.cs
+++ example/Assets/Standard Assets/Bugsnag/Bugsnag.cs
@@ -278,6 +278,7 @@ public class Bugsnag : MonoBehaviour {
     public static string[] SeverityValues = new string[]{"info", "error", "warning"};
 
     public string BugsnagApiKey = "";
+    public static string BugsnagApiKeyStatic = "";
     public bool AutoNotify = true;
 
     // Rate limiting section
@@ -368,6 +369,11 @@ public class Bugsnag : MonoBehaviour {
         maxCounts[unityLogType] = maxCount;
     }
 
+    // Used to set the API key when Bugsnag is being initialized
+    public static void SetApiKey(String apiKey) {
+        BugsnagApiKeyStatic = apiKey;
+    }
+
     string GetLevelName() {
 #if UNITY_5_OR_NEWER
       return SceneManager.GetActiveScene().name;
@@ -378,11 +384,34 @@ public class Bugsnag : MonoBehaviour {
 
     void Awake() {
         DontDestroyOnLoad(this);
-        Init(BugsnagApiKey);
+        Init(null);
     }
 
-    public void Init(string apiKey)
-    {
+    public static Bugsnag createBugsnagInstance(GameObject gameObject, String ApiKey) {
+        Bugsnag.SetApiKey(ApiKey);
+        Bugsnag bugsnagInstance = gameObject.AddComponent<Bugsnag>();
+        bugsnagInstance.Init(); 
+        return bugsnagInstance;
+    }
+
+    public void Init() {
+        Init(null);
+    }
+
+    public void Init(string apiKey) {
+        // Try to work out which API key to use
+        if (!String.IsNullOrEmpty (apiKey)) {
+            InitInternal (apiKey);
+        } else if (!String.IsNullOrEmpty(BugsnagApiKey)) {
+            InitInternal(BugsnagApiKey);
+        } else if (!String.IsNullOrEmpty(BugsnagApiKeyStatic)) {
+            InitInternal(BugsnagApiKeyStatic);
+        } else {
+            Debug.LogError("BUGSNAG: ERROR: unable to initialize Bugsnag, API key must be specified");
+        }
+    }
+
+    private void InitInternal(string apiKey) {
         BugsnagApiKey = apiKey;
         NativeBugsnag.Register(BugsnagApiKey);
 
