79a8fd9c4beca8ad5ad0fc4c627a2b2ab3b34e16
Delisa Mason <iskanamagus@gmail.com>
2017-10-03 22:57:43 +0100

    Release v3.5.0

--- bugsnag-android-unity/src/main/java/com/bugsnag/android/unity/UnityCallback.java
+++ bugsnag-android-unity/src/main/java/com/bugsnag/android/unity/UnityCallback.java
@@ -6,7 +6,7 @@ import com.bugsnag.android.*;
 
 class UnityCallback implements Callback {
     static final String NOTIFIER_NAME = "Bugsnag Unity (Android)";
-    static final String NOTIFIER_VERSION = "3.4.0";
+    static final String NOTIFIER_VERSION = "3.5.0";
     static final String NOTIFIER_URL = "https://github.com/bugsnag/bugsnag-unity";
 
     private final String context;
