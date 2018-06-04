0c2d9235ade3b07d1351ee7f04619064050b24d2
Martin Holman <martin@bugsnag.com>
2018-06-04 15:58:22 -0700

    Split this into its own class

--- src/Bugsnag.Unity/Client.cs
+++ src/Bugsnag.Unity/Client.cs
@@ -2,7 +2,6 @@
 using UnityEngine;
 using Bugsnag.Unity.Payload;
 using System.Collections.Generic;
-using System.Threading;
 using UnityEngine.SceneManagement;
 
 namespace Bugsnag.Unity
@@ -102,22 +101,5 @@ namespace Bugsnag.Unity
       ThreadQueueDelivery.Instance.Send(report);
       // add a breadcrumb about the report
     }
-  }
-
-  public class UnityMetadata
-  {
-    public static Dictionary<string, string> Data => new Dictionary<string, string> {
-      { "unityException", "false" },
-      { "unityVersion", Application.unityVersion },
-      { "platform", Application.platform.ToString() },
-      { "osLanguage", Application.systemLanguage.ToString() },
-      { "bundleIdentifier", Application.identifier },
-      //{ "bundleIdentifier", Application.bundleIdentifier },// this would seem to be the property in older versions of unity
-      { "version", Application.version },
-      { "companyName", Application.companyName },
-      { "productName", Application.productName },
-      { "threadId", Thread.CurrentThread.ManagedThreadId.ToString() },
-      { "threadName", Thread.CurrentThread.Name },
-    };
   }
 }
---
+++ src/Bugsnag.Unity/UnityMetadata.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Bugsnag.Unity
+{
+  public class UnityMetadata
+  {
+    public static Dictionary<string, string> Data => new Dictionary<string, string> {
+      { "unityException", "false" },
+      { "unityVersion", Application.unityVersion },
+      { "platform", Application.platform.ToString() },
+      { "osLanguage", Application.systemLanguage.ToString() },
+      { "bundleIdentifier", Application.identifier },
+      //{ "bundleIdentifier", Application.bundleIdentifier },// this would seem to be the property in older versions of unity
+      { "version", Application.version },
+      { "companyName", Application.companyName },
+      { "productName", Application.productName },
+      { "threadId", Thread.CurrentThread.ManagedThreadId.ToString() },
+      { "threadName", Thread.CurrentThread.Name },
+    };
+  }
+}
