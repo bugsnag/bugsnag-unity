4f94540cce2be6c169e38a0c818ab49e0b317ec6
Martin Holman <martin@bugsnag.com>
2018-06-05 16:30:14 -0700

    Add tests for our bait and switch setup
    These should ensure that all of the native dlls support the same api, not that the api works correctly with the native code

---
+++ tests/Bugsnag.Native.Tests/ClientTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Bugsnag.Native.Tests
+{
+  /// <summary>
+  /// These tests are about ensuring that the API for the native wrapper dlls
+  /// is consistent across the platforms. We are not testing functionality here
+  /// so we only need to call each of the methods that we want to test
+  /// </summary>
+  [TestFixture]
+  public class ClientTests
+  {
+    [Test]
+    public void RegisterWithApiKeyTest() => Client.Register("test");
+
+    [Test]
+    public void RegisterWithApiKeyAndAutoTrackSessionsTest() => Client.Register("test", false);
+  }
+}
