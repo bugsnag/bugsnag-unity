63fc4d0177406c0406cea5760695445210b08860
Martin Holman <martin@bugsnag.com>
2018-06-27 09:55:14 -0700

    Optimise this dictionary
    Removes the boxing that the dictionary would need to perform here

--- src/Bugsnag.Unity/Severity.cs
+++ src/Bugsnag.Unity/Severity.cs
@@ -18,11 +18,27 @@ namespace Bugsnag.Unity
   static class LogTypeExtensions
   {
     /// <summary>
+    /// Used to stop the dictionary boxing the value types
+    /// </summary>
+    class LogTypeComparer : IEqualityComparer<LogType>
+    {
+      public bool Equals(LogType x, LogType y)
+      {
+        return x == y;
+      }
+
+      public int GetHashCode(LogType obj)
+      {
+        return (int)obj;
+      }
+    }
+
+    /// <summary>
     /// The LogType enum from Unity doesn't have its enum values in an expected
     /// numerical order. This allows us to map them to something that we would
     /// expect.
     /// </summary>
-    private static Dictionary<LogType, int> LogTypeMapping { get; } = new Dictionary<LogType, int>
+    private static Dictionary<LogType, int> LogTypeMapping { get; } = new Dictionary<LogType, int>(5, new LogTypeComparer())
     {
       { LogType.Log, 0 },
       { LogType.Warning, 1 },
