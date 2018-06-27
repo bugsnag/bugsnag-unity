f7830eca96fa4909bd31b72152ef67b382586144
Martin Holman <martin@bugsnag.com>
2018-06-27 14:55:03 -0700

    Implement basic synchronous android http requests

--- src/Bugsnag.Unity/IDelivery.cs
+++ src/Bugsnag.Unity/IDelivery.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Text;
 using Bugsnag.Unity.Payload;
+using UnityEngine;
 
 namespace Bugsnag.Unity
 {
@@ -39,9 +38,121 @@ namespace Bugsnag.Unity
 
   class AndroidDelivery : IDelivery
   {
+    internal AndroidDelivery()
+    {
+      // ensure that the class loader has loaded some sort of java object or
+      // we will not be able to create any java objects on another thread
+      new AndroidJavaObject("java.lang.Object");
+    }
+
     public void Send(IPayload payload)
     {
+      // we need to ensure that the current thread is attached to the JVM
+      // this should be a no-op if it already is
+      AndroidJNI.AttachCurrentThread();
+
+      using (var url = new AndroidJavaObject("java.net.URL", payload.Endpoint.ToString()))
+      using (var connection = url.Call<AndroidJavaObject>("openConnection"))
+      {
+        try
+        {
+          connection.Call("setDoOutput", true);
+          connection.Call("setChunkedStreamingMode", 0);
+          connection.Call("addRequestProperty", "Content-Type", "application/json; charset=utf-8");
+          connection.Call("addRequestProperty", "Bugsnag-Sent-At", DateTime.UtcNow.ToString("o", CultureInfo.InvariantCulture));
+
+          foreach (var header in payload.Headers)
+          {
+            connection.Call("addRequestProperty", header.Key, header.Value);
+          }
+
+          using (var outputStream = connection.Call<AndroidJavaObject>("getOutputStream"))
+          using (var streamMapper = new JavaStreamWrapper(outputStream))
+          using (var writer = new StreamWriter(streamMapper, new UTF8Encoding(false)) { AutoFlush = false })
+          {
+            SimpleJson.SimpleJson.SerializeObject(payload, writer);
+            writer.Flush();
+          }
+
+          var code = connection.Call<int>("getResponseCode");
+        }
+        finally
+        {
+          connection.Call("disconnect");
+        }
+      }
+    }
+  }
+
+  class JavaStreamWrapper : Stream
+  {
+    public override bool CanRead => false;
+
+    public override bool CanSeek => false;
+
+    public override bool CanWrite => true;
+
+    public override long Length => throw new NotImplementedException();
+
+    public override long Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    AndroidJavaObject JavaStream { get; }
+
+    bool Disposed { get; set; }
+
+    public JavaStreamWrapper(AndroidJavaObject javaStream)
+    {
+      JavaStream = javaStream;
+    }
+
+    public override void Flush()
+    {
+      JavaStream.Call("flush");
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
       throw new NotImplementedException();
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+      throw new NotImplementedException();
+    }
+
+    public override void SetLength(long value)
+    {
+      throw new NotImplementedException();
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+      JavaStream.Call("write", buffer, offset, count);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+      base.Dispose(disposing);
+      if (!Disposed)
+      {
+        try
+        {
+          JavaStream.Call("close");
+        }
+        catch (System.Exception)
+        {
+          // match the bugsnag-android behaviour
+        }
+        finally
+        {
+          Disposed = true;
+        }
+      }
+    }
+
+    ~JavaStreamWrapper()
+    {
+      Dispose(false);
     }
   }
 }
