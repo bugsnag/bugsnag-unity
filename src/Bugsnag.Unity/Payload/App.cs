4ca2b9071753fcedd266991f9d12ac66564ec4f9
Martin Holman <martin@bugsnag.com>
2018-06-28 13:49:05 -0700

    Populate android app data

--- src/Bugsnag.Unity/Payload/App.cs
+++ src/Bugsnag.Unity/Payload/App.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Bugsnag.Unity.Payload
 {
@@ -17,6 +18,21 @@ namespace Bugsnag.Unity.Payload
       this.AddToPayload("version", version);
       this.AddToPayload("releaseStage", releaseStage);
       this.AddToPayload("type", type);
+    }
+  }
+
+  class AndroidApp : App
+  {
+    internal AndroidApp(IConfiguration configuration, AndroidJavaObject client) : base(configuration)
+    {
+      using (var app = client.Call<AndroidJavaObject>("getAppData"))
+      {
+        this.AddToPayload("id", app.Call<string>("getPackageName"));
+        this.AddToPayload("buildUUID", app.Call<string>("getBuildUuid"));
+        this.AddToPayload("duration", app.Call<long>("getDuration"));
+        this.AddToPayload("durationInForeground", app.Call<long>("getDurationInForeground"));
+        this.AddToPayload("inForeground", app.Call<bool>("isInForeground"));
+      }
     }
   }
 }
