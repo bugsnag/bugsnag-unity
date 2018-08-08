070ea8732c92ee81b0e635a003e108cc93f45130
Martin Holman <martin308@gmail.com>
2018-08-07 17:18:55 -0700

    Remove bundle identifier
    These are only relevant on native platforms where the native notifier
    will be able to retrieve the value anyway

--- src/BugsnagUnity/UnityMetadata.cs
+++ src/BugsnagUnity/UnityMetadata.cs
@@ -9,8 +9,6 @@ namespace BugsnagUnity
       { "unityVersion", Application.unityVersion },
       { "platform", Application.platform.ToString() },
       { "osLanguage", Application.systemLanguage.ToString() },
-      { "bundleIdentifier", Application.identifier },
-      //{ "bundleIdentifier", Application.bundleIdentifier },// this would seem to be the property in older versions of unity
       { "version", Application.version },
       { "companyName", Application.companyName },
       { "productName", Application.productName },
