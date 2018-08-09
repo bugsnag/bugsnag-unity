d35c3ecfd5b779d8e5a6add65bfd93005fad87f5
Martin Holman <martin@bugsnag.com>
2018-08-09 16:29:28 -0700

    Move these to their own directory and files

---
+++ src/BugsnagUnity/Clients/IClient.cs
@@ -0,0 +1,31 @@
+﻿using BugsnagUnity.Payload;
+
+namespace BugsnagUnity
+{
+  public delegate void Middleware(Report report);
+
+  public interface IClient
+  {
+    IConfiguration Configuration { get; }
+
+    IBreadcrumbs Breadcrumbs { get; }
+
+    ISessionTracker SessionTracking { get; }
+
+    User User { get; }
+
+    void Send(IPayload payload);
+
+    Metadata Metadata { get; }
+
+    void BeforeNotify(Middleware middleware);
+
+    void Notify(System.Exception exception);
+
+    void Notify(System.Exception exception, Middleware callback);
+
+    void Notify(System.Exception exception, Severity severity);
+
+    void Notify(System.Exception exception, Severity severity, Middleware callback);
+  }
+}
