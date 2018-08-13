0f50aee58f48ad01ce1b9745735144b8de6b1b1a
Martin Holman <martin308@gmail.com>
2018-08-13 14:57:05 -0700

    Add maze-runner gem

---
+++ features/support/env.rb
@@ -0,0 +1,17 @@
+# Any 'run once' setup should go here as this file is evaluated
+# when the environment loads.
+# Any helper functions added here will be available in step
+# definitions
+
+# Scenario hooks
+Before do
+# Runs before every Scenario
+end
+
+After do
+# Runs after every Scenario
+end
+
+at_exit do
+# Runs when the test run is completed
+end
