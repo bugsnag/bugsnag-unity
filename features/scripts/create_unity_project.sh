ea05760a1a6e661809e0f9710a37f9446d4351cc
Martin Holman <martin308@gmail.com>
2018-08-15 10:22:36 -0700

    Fully qualify all paths for Unity 2017

--- features/scripts/create_unity_project.sh
+++ features/scripts/create_unity_project.sh
@@ -7,11 +7,12 @@ pushd "${0%/*}"
     package_path=`pwd`
   popd
   pushd ../fixtures
-    rm -rf unity_project
-    Unity -nographics -quit -batchmode -logFile unity.log -createProject unity_project
-    Unity -nographics -quit -batchmode -logFile unity.log -projectpath unity_project -importPackage "$package_path/Bugsnag.unitypackage"
+    git clean -xdf .
+    project_path="$(pwd)/unity_project"
+    Unity -nographics -quit -batchmode -logFile unity.log -createProject $project_path
+    Unity -nographics -quit -batchmode -logFile unity.log -projectpath $project_path -importPackage "$package_path/Bugsnag.unitypackage"
     cp Main.cs unity_project/Assets/Main.cs
-    BUGSNAG_APIKEY=a35a2a72bd230ac0aa0f52715bbdc6aa Unity -nographics -quit -batchmode -logFile unity.log -projectpath unity_project -executeMethod "Main.CreateScene"
-    Unity -nographics -quit -batchmode -logFile unity.log -projectpath unity_project -buildOSXUniversalPlayer "$package_path/features/fixtures/Mazerunner.app"
+    BUGSNAG_APIKEY=a35a2a72bd230ac0aa0f52715bbdc6aa Unity -nographics -quit -batchmode -logFile unity.log -projectpath $project_path -executeMethod "Main.CreateScene"
+    Unity -nographics -quit -batchmode -logFile unity.log -projectpath $project_path -buildOSXUniversalPlayer "$package_path/features/fixtures/Mazerunner.app"
   popd
 popd
