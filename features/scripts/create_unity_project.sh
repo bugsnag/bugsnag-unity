5fd25b2a1beb8cf2bc34fe81bf6016bcb90267e8
Martin Holman <martin308@gmail.com>
2018-08-15 12:53:36 -0700

    Remove api key env variable, set elsewhere

--- features/scripts/create_unity_project.sh
+++ features/scripts/create_unity_project.sh
@@ -12,7 +12,7 @@ pushd "${0%/*}"
     Unity -nographics -quit -batchmode -logFile unity.log -createProject $project_path
     Unity -nographics -quit -batchmode -logFile unity.log -projectpath $project_path -importPackage "$package_path/Bugsnag.unitypackage"
     cp Main.cs unity_project/Assets/Main.cs
-    BUGSNAG_APIKEY=a35a2a72bd230ac0aa0f52715bbdc6aa Unity -nographics -quit -batchmode -logFile unity.log -projectpath $project_path -executeMethod "Main.CreateScene"
+    Unity -nographics -quit -batchmode -logFile unity.log -projectpath $project_path -executeMethod "Main.CreateScene"
     Unity -nographics -quit -batchmode -logFile unity.log -projectpath $project_path -buildOSXUniversalPlayer "$package_path/features/fixtures/Mazerunner.app"
   popd
 popd
