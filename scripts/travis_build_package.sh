99a6bebb1418340e431e811659a0b39ed755db62
Martin Holman <martin308@gmail.com>
2018-10-02 11:03:44 -0700

    Install ndk
    We need this to build the android library now

--- scripts/travis_build_package.sh
+++ scripts/travis_build_package.sh
@@ -10,6 +10,11 @@ export PATH="$PATH:/Library/Frameworks/Mono.framework/Versions/Current/Commands"
 yes | sdkmanager "platforms;android-27"
 yes | sdkmanager --licenses
 
+curl -o ndk.zip https://dl.google.com/android/repository/android-ndk-r16b-darwin-x86_64.zip
+unzip -qq ndk.zip
+mv android-ndk-r16b $ANDROID_NDK_HOME
+rm ndk.zip
+
 bundle exec rake travis:export_plugin
 
 # copy it to the directory that is being synchronised with S3
