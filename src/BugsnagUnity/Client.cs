7f6272acd47888684c12fce18f28f7f315aaa04a
Martin Holman <martin308@gmail.com>
2018-10-10 17:09:53 -0700

    Populate app and device sections using native client data

--- src/BugsnagUnity/Client.cs
+++ src/BugsnagUnity/Client.cs
@@ -29,7 +29,7 @@ namespace BugsnagUnity
 
     object MiddlewareLock { get; } = new object();
 
-    INativeClient NativeClient { get; }
+    internal INativeClient NativeClient { get; }
 
     Stopwatch Stopwatch { get; }
 
