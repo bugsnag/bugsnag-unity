af6e92489e2456563c667bf8f86f71db3d5a71ef
Delisa Mason <iskanamagus@gmail.com>
2018-10-30 17:33:50 -0700

    ci: Shh

--- scripts/travis_build_package.sh
+++ scripts/travis_build_package.sh
@@ -8,7 +8,7 @@ brew cask install \
 export PATH="$PATH:/Library/Frameworks/Mono.framework/Versions/Current/Commands"
 
 yes | sdkmanager "platforms;android-27" > /dev/null
-yes | sdkmanager --licenses
+yes | sdkmanager --licenses > /dev/null
 
 curl --silent -o ndk.zip https://dl.google.com/android/repository/android-ndk-r16b-darwin-x86_64.zip
 unzip -qq ndk.zip > /dev/null
