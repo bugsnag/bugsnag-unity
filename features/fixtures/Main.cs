ba1cfd2b446f3664e548a72fa7de24b01b4b9861
Delisa Mason <iskanamagus@gmail.com>
2018-12-20 10:46:28 +0000

    tests: Make double notify scenarios consistent
    They can periodically fail, this increases the wait time to longer than
    the dequeuing time for the delivery thread.

--- features/fixtures/Main.cs
+++ features/fixtures/Main.cs
@@ -83,7 +83,7 @@ public class Main : MonoBehaviour {
 
   void NotifyTwice() {
     Bugsnag.Notify(new System.Exception("Rollback failed"));
-    System.Threading.Thread.Sleep(20);
+    System.Threading.Thread.Sleep(2000);
     Bugsnag.Notify(new ExecutionEngineException("Invalid runtime"));
   }
 
