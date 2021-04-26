b1c4f7ca550c05cea1c6cb64b4ffb548df14418a
Steve Kirkland <steve@bugsnag.com>
2021-04-26 19:34:39 +0100

    Improve script logging and error detection

--- features/scripts/create_unity_project.sh
+++ features/scripts/create_unity_project.sh
@@ -2,6 +2,7 @@
 
 export PATH="/Applications/Unity/Hub/Editor/$UNITY_VERSION/Unity.app/Contents/MacOS:$PATH"
 
+# Set project_path to the repo root
 pushd "${0%/*}"
   pushd ../..
     package_path=`pwd`
@@ -11,12 +12,16 @@ pushd "${0%/*}"
     log_file="$package_path/unity.log"
     project_path="$(pwd)/unity_project"
 
+    echo "Creating Unity project in $project_path"
     Unity -nographics -quit -batchmode -logFile $log_file \
       -createProject $project_path
 
+    echo "Importing Bugsnag package $package_path/Bugsnag.unitypackage"
     Unity -nographics -quit -batchmode -logFile $log_file \
       -projectPath $project_path \
       -importPackage "$package_path/Bugsnag.unitypackage"
+    RESULT=$?
+    if [ $RESULT -ne 0 ]; then exit $RESULT; fi
 
     cp Main.cs unity_project/Assets/Main.cs
     cp -R NativeCrashy.bundle unity_project/Assets/Plugins/OSX/NativeCrashy.bundle
@@ -24,9 +29,13 @@ pushd "${0%/*}"
     Unity -nographics -quit -batchmode -logFile $log_file \
       -projectPath $project_path \
       -executeMethod "Main.CreateScene"
+    RESULT=$?
+    if [ $RESULT -ne 0 ]; then exit $RESULT; fi
 
     Unity -nographics -quit -batchmode -logFile - \
       -projectPath $project_path \
       -buildOSXUniversalPlayer "$(pwd)/Mazerunner.app"
+    RESULT=$?
+    if [ $RESULT -ne 0 ]; then exit $RESULT; fi
   popd
 popd
