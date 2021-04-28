93e74fcbc63d17ea162d292fbde8ec643443d05f
Steve Kirkland <steve@bugsnag.com>
2021-04-28 22:33:24 +0100

    Preliminary commit for mobile test fixture

---
+++ test/mobile/Assets/ReporterBehavior.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using BugsnagUnity;
+using System.Collections.Generic;
+using BugsnagUnity.Payload;
+using UnityEngine.SceneManagement;
+
+public class ReporterBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    
+    public void ThrowException() {
+        throw new System.Exception("You threw an exception!");
+    }
+    
+    public void AssertionFailure() {
+        var items = new int[]{1, 2, 3};
+        Debug.Log("Item4 is: " + items[4]);
+    }
+    
+    public void NativeException() {
+        BugsnagNative.Crash();
+    }
+    
+    public void LogCaughtException() {
+        try {
+            var items = new int[]{1, 2, 3};
+            Debug.Log("Item4 is: " + items[4]);
+        } catch (System.Exception ex) {
+            Debug.LogException(ex);
+        }
+    }
+    
+    public void LogWithClasPrefix() {
+        throw new ExecutionEngineException("Haven't gotten around to making this work, sorry");
+    }    
+    public void NotifyCaughtException() {
+        try {
+            var items = new int[]{1, 2, 3};
+            Debug.Log("Item4 is: " + items[4]);
+        } catch (System.Exception ex) {
+            Bugsnag.Notify(ex);
+        }
+    }    
+    public void NotifyWithCallback() {
+        Bugsnag.Notify(new ExecutionEngineException("This one has a callback"), report =>
+        {
+            report.Context = "Callback Context";
+            report.Metadata.Add("Callback", new Dictionary<string, string>()
+            {
+                {"region", "US"}
+            });
+        });
+    }    
+    public void StartSession() {
+        Bugsnag.SessionTracking.StartSession();
+    }    
+    public void SetReleaseStage() {
+        Bugsnag.Configuration.ReleaseStage = "CustomStage";
+    }    
+    public void SetContext() {
+        Bugsnag.Configuration.Context = "Configured Context";
+    }    
+    public void SetUser() {
+        Bugsnag.User.Id = "mcpacman";
+        Bugsnag.User.Name = "Geordi McPacman";
+        Bugsnag.User.Email = "configureduser@example.com";
+    }    
+    public void ClearUser() {
+        Bugsnag.User.Clear();
+    }    
+    public void SetAppVersion() {
+        Bugsnag.Configuration.AppVersion = "2.2.300-config";
+    }    
+    public void AddMetadata() {
+        Bugsnag.Metadata.Add("ConfigMetadata", new Dictionary<string, string>(){
+          { "subsystem", "Player Mechanics" }
+        });
+    }    
+    public void ClearMetadata() {
+        Bugsnag.Metadata.Clear();
+    }    
+    public void AddCallbackMetadata() {
+        Bugsnag.BeforeNotify(report =>
+        {
+            report.Metadata.Add("CallbackMetadata", new Dictionary<string, string>(){
+                { "subsystem", "Player Mechanics" }
+            });
+        });
+    }    
+    public void AddCallbackContext() {
+        Bugsnag.BeforeNotify(report =>
+        {
+            report.Context = "BeforeNotify Context";
+        });
+    }    
+    public void AddCallbackUser() {
+        Bugsnag.BeforeNotify(report =>
+        {
+            report.User.Id = "lunchfrey";
+            report.User.Name = "Lunchfrey Jones";
+            report.User.Email = "beforenotifyuser@example.com";
+        });
+    }    
+    public void AddCallbackSeverity() {
+        Bugsnag.BeforeNotify(report =>
+        {
+            report.Severity = Severity.Info;
+        });
+    }    
+    public void AddCallbackCancellation() {
+        Bugsnag.BeforeNotify(report =>
+        {
+            report.Ignore();
+        });
+    }    
+    public void RemoveAllCallbacks() {
+        throw new ExecutionEngineException("Hmm, doesn't seem to exist");
+    }    
+    public void LeaveBreadcrumbString() {
+        Bugsnag.Breadcrumbs.Leave("String breadcrumb clicked");
+    }    
+    public void LeaveBreadcrumbTuple() {
+        Bugsnag.Breadcrumbs.Leave(
+          "Tuple breadcrumb clicked",
+          BreadcrumbType.Navigation,
+          new Dictionary<string, string>() {{ "scene", "SomeVeryRealScene" }}
+        );
+    }
+    public void ChangeScene() {
+        SceneManager.LoadScene("OtherScene");
+    }
+}
---
+++ test/mobile/Assets/Scripts/BugsnagNative.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices; 
+using UnityEngine;
+
+public class BugsnagNative {
+
+	#if UNITY_IOS || UNITY_EDITOR_OSX || UNITY_TVOS || UNITY_OSX
+	[DllImport ("__Internal")]
+	private static extern void framework_crash_me();
+	#endif
+
+	public static void Crash()
+	{
+		switch (Application.platform)
+		{
+			case RuntimePlatform.IPhonePlayer:
+            case RuntimePlatform.OSXPlayer:
+            case RuntimePlatform.tvOS:
+				BugsnagNative.IOSCrash ();
+				break;
+			case RuntimePlatform.Android:
+				BugsnagNative.AndroidCrash ();
+				break;
+			default:
+				BugsnagNative.UnhandledCrash ();
+				break;
+		}
+	}
+
+	private static void IOSCrash()
+	{
+		#if UNITY_IOS || UNITY_EDITOR_OSX || UNITY_TVOS || UNITY_OSX
+		framework_crash_me();
+		#endif
+	}
+
+	private static void AndroidCrash()
+	{
+		AndroidJavaClass crashClass = new AndroidJavaClass ("com.example.bugsnagcrashplugin.CrashHelper");
+		crashClass.CallStatic ("UnhandledCrash");
+	}
+
+	private static void UnhandledCrash()
+	{
+		Debug.Log ("Generating UnhandledCrash");
+		int x = 0;
+		int y = 42;
+		int z = y / x;
+		Debug.Log (z.ToString ());
+	}
+}
