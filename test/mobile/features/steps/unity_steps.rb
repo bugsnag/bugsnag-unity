4f26c8ddeb787e4b8f424419260df7971ab2e0a7
Steve Kirkland <steve@bugsnag.com>
2021-05-01 22:50:47 +0100

    Clicks button

--- test/mobile/features/steps/unity_steps.rb
+++ test/mobile/features/steps/unity_steps.rb
@@ -6,13 +6,18 @@ When("I say {string}") do |message|
 end
 
 When("I tap the {string} button") do |button|
+  # Specific to the ANDROID_9_0 Pixel 3
+  STDOUT.puts button
+  middle = 2160 / 2
   case button
   when "throw Exception"
-    tap 150, 150
+    STDOUT.puts "tap it"
+    press_at 540, middle - 750
   end
 end
 
-def tap(x, y)
+def press_at(x, y)
+  STDOUT.puts "tap #{x}. #{y}"
   touch_action = Appium::TouchAction.new
   touch_action.tap({:x => x, :y => y})
   touch_action.perform
