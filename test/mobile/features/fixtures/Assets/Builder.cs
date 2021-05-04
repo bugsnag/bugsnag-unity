dfb6654e4d0957f62d056dd63d9bfb77264a4243
Steve Kirkland <steve@bugsnag.com>
2021-05-04 21:58:42 +0100

    Add Builder class

---
+++ test/mobile/features/fixtures/Assets/Builder.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR // all of the build tools-related stuff does not exist at runtime in the app
+using UnityEditor;
+
+class Sample : MonoBehaviour
+{
+
+    static void Build(string folder, BuildTarget target)
+    {
+        BuildPlayerOptions opts = new BuildPlayerOptions();
+        opts.scenes = new[] { "Assets/Scenes/SampleScene.unity", "Assets/Scenes/OtherScene.unity" };
+        opts.locationPathName = folder;
+        opts.target = target;
+        opts.options = BuildOptions.None;
+
+        BuildPipeline.BuildPlayer(opts);
+    }
+
+    // Generates iOSBuild/Unity-iPhone.xcodeproj, which can then be used to make an .ipa using
+    // xcodebuild
+    static void iOSBuild()
+    {
+        Debug.Log("Building iOS app...");
+        Build("iOSBuild", BuildTarget.iOS);
+    }
+
+    // Generates AndroidBuild/MyApp.apk
+    static void AndroidBuild()
+    {
+        Debug.Log("Building Android app...");
+        Build("AndroidBuild", BuildTarget.Android);
+    }
+}
+#endif
