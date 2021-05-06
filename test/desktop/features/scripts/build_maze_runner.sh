000ed96a9b7431d5c9d94f5f377f1a8d628c95c8
Steve Kirkland <steve@bugsnag.com>
2021-05-06 10:35:27 +0100

    Temp checkin for debug

--- test/desktop/features/scripts/build_maze_runner.sh
+++ test/desktop/features/scripts/build_maze_runner.sh
@@ -9,33 +9,32 @@ pushd "${0%/*}"
     echo "Expecting to find Busnag package in: $package_path"
   popd
   pushd ../fixtures
-    git clean -xdf .
     log_file="$package_path/unity.log"
     project_path="$(pwd)/maze_runner"
 
-    echo "Creating Unity project in $project_path"
-    Unity -nographics -quit -batchmode -logFile $log_file \
-      -createProject $project_path
-
-    echo "Importing Bugsnag package $package_path/Bugsnag.unitypackage"
+    echo "Importing $package_path/Bugsnag.unitypackage into $project_path"
     Unity -nographics -quit -batchmode -logFile $log_file \
       -projectPath $project_path \
       -importPackage "$package_path/Bugsnag.unitypackage"
     RESULT=$?
     if [ $RESULT -ne 0 ]; then exit $RESULT; fi
+    exit 0
 
-    cp -R NativeCrashy.bundle maze_runner/Assets/Plugins/OSX/NativeCrashy.bundle
+#    cp -R NativeCrashy.bundle maze_runner/Assets/Plugins/OSX/NativeCrashy.bundle
 
-    Unity -nographics -quit -batchmode -logFile $log_file \
-      -projectPath $project_path \
-      -executeMethod "Main.CreateScene"
-    RESULT=$?
-    if [ $RESULT -ne 0 ]; then exit $RESULT; fi
+#    Unity -nographics -quit -batchmode -logFile $log_file \
+#      -projectPath $project_path \
+#      -executeMethod "Main.CreateScene"
+#    RESULT=$?
+#    if [ $RESULT -ne 0 ]; then exit $RESULT; fi
 
-    Unity -nographics -quit -batchmode -logFile - \
-      -projectPath $project_path \
-      -buildOSXUniversalPlayer "$(pwd)/Mazerunner.app"
-    RESULT=$?
-    if [ $RESULT -ne 0 ]; then exit $RESULT; fi
+#    Unity -nographics -quit -batchmode -logFile - \
+#      -projectPath $project_path \
+#      -buildOSXUniversalPlayer "$(pwd)/Mazerunner.app"
+#    RESULT=$?
+#    if [ $RESULT -ne 0 ]; then exit $RESULT; fi
   popd
 popd
+
+
+    Unity -nographics -quit -batchmode -projectPath "fixtures/maze_runner" -importPackage "../../../Bugsnag.unitypackage"
