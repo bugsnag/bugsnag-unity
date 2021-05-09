3f5177060b73f866960da9613c78fce2362db0ed
Steve Kirkland <steve@bugsnag.com>
2021-05-09 22:51:40 +0100

    Allow test fixture to use http

--- test/mobile/features/scripts/build_fixture.sh
+++ test/mobile/features/scripts/build_fixture.sh
@@ -19,10 +19,10 @@ Unity $DEFAULT_CLI_ARGS -createProject `pwd`/maze_runner
 Unity $DEFAULT_CLI_ARGS -importPackage $script_path/../../../../Bugsnag.unitypackage
 Unity $DEFAULT_CLI_ARGS -importPackage $script_path/../../../../Bugsnag-with-android-64bit.unitypackage
 
-
 cp -r Assets/Scenes maze_runner/Assets/
 cp -r Assets/Scripts maze_runner/Assets/
 cp Assets/Editor/Builder.cs maze_runner/Assets/Scripts/
+cp Assets/Plugins/Android/AndroidManifest.xml maze_runner/Assets/Plugins/Android
 
 # Running a custom script - must reference a static method
 Unity $DEFAULT_CLI_ARGS -executeMethod Builder.AndroidBuild -projectPath `pwd`/maze_runner
