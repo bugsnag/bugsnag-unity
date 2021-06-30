647ede84a15e9cfbd1a3e0ac36d0a0e746689da1
Richard Elms <rich@bugsnag.com>
2021-06-30 09:52:31 +0200

    review feedback tested on windows, still needs macos testing

--- test/desktop/features/scripts/build_maze_runner.sh
+++ test/desktop/features/scripts/build_maze_runner.sh
@@ -5,25 +5,20 @@ if [ -z "$UNITY_VERSION" ]; then
   exit 1
 fi
 
-
 #!/usr/bin/env bash
 
 if [ "$(uname)" == "Darwin" ]; then
-   PLATFORM="MacOS"    
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    PLATFORM="Win64"  
-fi
 
-if [ "$PLATFORM" == "MacOS" ]; then
-
+  PLATFORM="MacOS"    
   echo "MacOS Detected"
   UNITY_PATH="/Applications/Unity/Hub/Editor/$UNITY_VERSION/Unity.app/Contents/MacOS/Unity"
 
-else
+elif [ $OS == "Windows_NT" ]; then
 
-  set -m
-  echo "Windows64 Detected"
-  UNITY_PATH="/c/Program Files/Unity/Hub/Editor/$UNITY_VERSION/Editor/Unity.exe" 
+    PLATFORM="Win64"  
+    set -m
+    echo "Windows64 Detected"
+    UNITY_PATH="/c/Program Files/Unity/Hub/Editor/$UNITY_VERSION/Editor/Unity.exe" 
 
 fi
 
@@ -61,8 +56,8 @@ pushd "${0%/*}"
     RESULT=$?
     if [ $RESULT -ne 0 ]; then exit $RESULT; fi
 
-    app_location="$(pwd)/Mazerunner.app"
-    echo "Building $app_location"
+    
+    echo "Building the fixture for $PLATFORM"
 
     "$UNITY_PATH" $DEFAULT_CLI_ARGS \
       -projectPath $project_path \
@@ -78,11 +73,9 @@ pushd "${0%/*}"
          if [ $RESULT -ne 0 ]; then exit $RESULT; fi
 
     else
-      echo "SHOULD ZIP BUILD"
        tar -czf "Mazerunner-$UNITY_VERSION.zip" "maze_runner/WindowsBuild/"
         RESULT=$?
          if [ $RESULT -ne 0 ]; then exit $RESULT; fi
-
     fi
 
 
