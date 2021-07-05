7b0b1cbc43954f0b1496aa1de4e99a54aeff3261
Steve Kirkland <steve@bugsnag.com>
2021-07-05 15:31:01 +0100

    Ensure —os option is provided

--- test/desktop/features/support/env.rb
+++ test/desktop/features/support/env.rb
@@ -2,21 +2,23 @@ require 'fileutils'
 
 $api_key = 'a35a2a72bd230ac0aa0f52715bbdc6aa'
 
+raise '--os option must be set (to "macos" or "windows"' if Maze.config.os.nil?
+
+AfterConfiguration do |_config|
+  Maze.config.enforce_bugsnag_integrity = false
+
+  if Maze.config.os.downcase == 'macos'
+    # The default macOS Crash Reporter "#{app_name} quit unexpectedly" alert grabs focus which can cause tests to flake.
+    # This option, which appears to have been introduced in macOS 10.11, displays a notification instead of the alert.
+    Maze::Runner.run_command('defaults write com.apple.CrashReporter UseUNC 1')
+  end
+end
+
 Maze.hooks.before do
   if Maze.config.os == 'macos'
     support_dir = File.expand_path '~/Library/Application Support/com.bugsnag.Bugsnag'
     $logger.info 'Clearing #{support_dir}'
     FileUtils.rm_rf(support_dir)
-  end
-end
-
-AfterConfiguration do |_config|
-  Maze.config.enforce_bugsnag_integrity = false
-
-  if Maze.config.os == 'macos'
-    # The default macOS Crash Reporter "#{app_name} quit unexpectedly" alert grabs focus which can cause tests to flake.
-    # This option, which appears to have been introduced in macOS 10.11, displays a notification instead of the alert.
-    Maze::Runner.run_command('defaults write com.apple.CrashReporter UseUNC 1')
   end
 end
 
