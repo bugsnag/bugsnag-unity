67d4ac740d27b17ef2a97a4f53bc2a3166c4663f
Steve Kirkland <steve@bugsnag.com>
2021-07-07 18:41:05 +0100

    Run e2e tests on Windows

---
+++ scripts/ci-run-windows-tests.bat
@@ -0,0 +1,7 @@
+REM Using the artifacts plugin v1.3 on Windows seems to break the whole step
+buildkite-agent artifact download test/desktop/features/fixtures/maze_runner/WindowsBuild-%UNITY_VERSION%.zip
+cd test\desktop\features\fixtures\maze_runner
+7z x WindowsBuild-%UNITY_VERSION%.zip
+cd ..\..\..
+wsl -d Ubuntu bundle install
+wsl -d bundle exec maze-runner --app=features/fixtures/maze_runner/WindowsBuild/Mazerunner.exe --os=windows
