0a8bb898b7078d85c1b584e1b571387d7df456ab
Steve Kirkland <steve@bugsnag.com>
2021-07-13 08:17:34 +0100

    Feed result of test run back to Buildkite

--- scripts/ci-run-macos-tests.sh
+++ scripts/ci-run-macos-tests.sh
@@ -4,5 +4,7 @@ pushd features/fixtures/maze_runner
   pushd ../../..
     bundle install
     bundle exec maze-runner --app=features/fixtures/maze_runner/Mazerunner.app --os=macos
+    RESULT=$?
   popd
 popd
+exit $RESULT
