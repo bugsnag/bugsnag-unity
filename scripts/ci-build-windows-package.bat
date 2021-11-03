7ffc662e59f627a195ba653ce254d8d4b3526b73
Steve Kirkland-Walton <steve@bugsnag.com>
2021-11-03 14:30:57 +0000

    Return exit code from batch script if build fails [full ci]

--- scripts/ci-build-windows-package.bat
+++ scripts/ci-build-windows-package.bat
@@ -2,5 +2,7 @@ REM Using the artifacts plugin v1.3 on Windows seems to break the whole step
 buildkite-agent artifact download "Bugsnag.unitypackage" .
 cd features\scripts
 C:\Progra~1\Git\bin\bash.exe build_maze_runner.sh windows
+IF NOT [%ERRORLEVEL%] EQU [0] EXIT /B %ERRORLEVEL%
+
 cd ..\fixtures\maze_runner\build
 7z a -r Windows-%UNITY_VERSION%.zip Windows
