2dda3846df0c7f97d374169be837f5491f5bb1c9
Richard Elms <rich@bugsnag.com>
2022-01-20 17:16:55 +0100

    initial filemanager

---
+++ src/BugsnagUnity/FileManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using BugsnagUnity.Payload;
+using UnityEngine;
+namespace BugsnagUnity
+{
+    public class FileManager
+    {
+
+        private static string CacheDirectory
+        {
+            get { return Application.temporaryCachePath + "/Bugsnag"; }
+        }
+
+
+        private static string SessionsDirectory
+        {
+            get { return CacheDirectory + "/Sessions"; }
+        }
+
+
+        internal static void CacheSession(Session session)
+        {
+            if (session != null)
+            {
+                using (var stream = new MemoryStream())
+                using (var reader = new StreamReader(stream))
+                using (var writer = new StreamWriter(stream, new UTF8Encoding(false)) { AutoFlush = false })
+                {
+                    SimpleJson.SerializeObject(session.Payload, writer);
+                    writer.Flush();
+                    stream.Position = 0;
+                    var jsonString = reader.ReadToEnd();
+                    
+                }
+            }
+        }
+
+    }
+}
+var tw = new StreamWriter(GetSaveFilePath());
+if (_config.ScrambleSaveData)
+{
+    data = DataScrambler(data);
+}
+tw.Write(data);
+tw.Close();
