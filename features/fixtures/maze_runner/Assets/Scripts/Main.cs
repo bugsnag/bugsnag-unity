6de17f98960f6a350bfa6489c4a77617ed394367
rich-bugsnag <83639642+rich-bugsnag@users.noreply.github.com>
2022-02-04 12:17:22 +0100

    Update features/fixtures/maze_runner/Assets/Scripts/Main.cs
    Co-authored-by: Jason <lemnik@users.noreply.github.com>

--- features/fixtures/maze_runner/Assets/Scripts/Main.cs
+++ features/fixtures/maze_runner/Assets/Scripts/Main.cs
@@ -152,13 +152,13 @@ public class Main : MonoBehaviour
         switch (scenario)
         {
             case "FeatureFlagsConfigClearAll":
-                config.AddFeatureFlag("testName1", "testVarient1");
-                config.AddFeatureFlag("testName2", "testVarient2");
+                config.AddFeatureFlag("testName1", "testVariant1");
+                config.AddFeatureFlag("testName2", "testVariant2");
                 config.ClearFeatureFlags();
                 break;
             case "FeatureFlagsInConfig":
-                config.AddFeatureFlag("testName1", "testVarient1");
-                config.AddFeatureFlag("testName2", "testVarient2");
+                config.AddFeatureFlag("testName1", "testVariant1");
+                config.AddFeatureFlag("testName2", "testVariant2");
                 config.ClearFeatureFlag("testName1");
                 break;
             case "DisableErrorBreadcrumbs":
