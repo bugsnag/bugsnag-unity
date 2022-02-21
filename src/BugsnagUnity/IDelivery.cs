d66325d9cf981ece6c91b8aa8331dddc7c519ef6
Richard Elms <rich@bugsnag.com>
2022-02-21 22:11:14 +0100

    Try sending on other session send [full ci]

--- src/BugsnagUnity/IDelivery.cs
+++ src/BugsnagUnity/IDelivery.cs
@@ -120,6 +120,7 @@ namespace BugsnagUnity
                 {
                     // success!
                     FileManager.PayloadSendSuccess(payload);
+                    TrySendingCachedPayloads();
                 }
                 else if (req.responseCode >= 500)
                 {
