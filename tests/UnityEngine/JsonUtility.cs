f81e9db8712424242f2a82aaad494375445b2530
Richard Elms <rich@bugsnag.com>
2022-03-01 15:56:31 +0100

    task(device): persist device id

---
+++ tests/UnityEngine/JsonUtility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace UnityEngine
+{
+    public class JsonUtility
+    {
+        public JsonUtility()
+        {
+        }
+        public static T FromJson<T>(string json) {
+            var t = new object();
+            return (T)t;
+        }
+        public static string ToJson(object obj) {
+            return string.Empty;
+        }
+
+
+    }
+}
