0b0588a0e67c5c7c56f4ea3d1efebba5309fbcb7
Steve Kirkland-Walton <steve@bugsnag.com>
2022-06-30 22:16:14 +0100

    Fixes

--- scripts/ci-build-windows-fixture-wsl.sh
+++ scripts/ci-build-windows-fixture-wsl.sh
@@ -1,14 +1,5 @@
-REM Using the artifacts plugin v1.3 on Windows seems to break the whole step
-buildkite-agent artifact download "Bugsnag.unitypackage" .
-cd features\scripts
-C:\Progra~1\Git\bin\bash.exe build_maze_runner.sh windows
-IF NOT [%ERRORLEVEL%] EQU [0] EXIT /B %ERRORLEVEL%
-
-cd ..\fixtures\maze_runner\build
-7z a -r Windows-%UNITY_VERSION%.zip Windows
-
 #!/bin/bash -e
 cd features/scripts
-./build_maze_runner.sh windows
+./build_maze_runner.sh wsl
 cd ../fixtures/maze_runner/build
-zip -r Windows-$(UNITY_VERSION).zip Windows
+zip -r Windows-$UNITY_VERSION.zip Windows
