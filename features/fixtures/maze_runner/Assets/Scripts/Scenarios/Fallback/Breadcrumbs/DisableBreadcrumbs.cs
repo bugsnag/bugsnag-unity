ea1fe04c34525ab1400bf3e781674dbf00ef4996
Richard Elms <rich@bugsnag.com>
2022-10-05 12:53:43 +0200

    Disable Breadcrumbs

---
+++ features/fixtures/maze_runner/Assets/Scripts/Scenarios/Fallback/Breadcrumbs/DisableBreadcrumbs.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BugsnagUnity;
+using BugsnagUnity.Payload;
+using System;
+
+public class DisableBreadcrumbs : Scenario
+{
+    public override void PrepareConfig(string apiKey, string host)
+    {
+        base.PrepareConfig(apiKey, host);
+        Configuration.EnabledBreadcrumbTypes = new BreadcrumbType[0];
+    }
+
+    public override void Run()
+    {
+        base.Run();
+        Bugsnag.Notify(new Exception("DisableBreadcrumbs"));
+    }
+}
