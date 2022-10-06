59cbcf956c375f1d199c3a4d0271ba1a8dc74cf6
Richard Elms <rich@bugsnag.com>
2022-10-06 14:52:49 +0200

    pipeline tweaks

---
+++ scripts/ci-run-macos-tests-csharp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+pushd features/fixtures/maze_runner/build
+  unzip MacOS-$UNITY_VERSION.zip
+popd
+
+bundle install
+bundle exec maze-runner --app=features/fixtures/maze_runner/build/MacOS/Mazerunner.app --os=macos features/csharp
