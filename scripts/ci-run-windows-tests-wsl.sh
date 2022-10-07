5ee389d0376876693c10544d1cedf4a38dcf8694
Richard Elms <rich@bugsnag.com>
2022-10-07 10:45:19 +0200

    run webgl tests

--- scripts/ci-run-windows-tests-wsl.sh
+++ scripts/ci-run-windows-tests-wsl.sh
@@ -3,4 +3,4 @@ cd features/fixtures/maze_runner/build
 unzip Windows-$UNITY_VERSION.zip
 cd ../../../..
 bundle install
-bundle exec maze-runner --app=features/fixtures/maze_runner/build/Windows/Mazerunner.exe --os=windows features/desktop
+bundle exec maze-runner --app=features/fixtures/maze_runner/build/Windows/Mazerunner.exe --os=windows features/csharp
