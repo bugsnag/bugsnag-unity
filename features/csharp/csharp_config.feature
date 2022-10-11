5de341bd6371267ce81d5d5508a8b4b0e0ac6c2b
Richard Elms <rich@bugsnag.com>
2022-10-11 11:44:31 +0200

    MarkLaunchComplete

--- features/csharp/csharp_config.feature
+++ features/csharp/csharp_config.feature
@@ -7,5 +7,10 @@ Feature: csharp events
     And the exception "message" equals "InfiniteLaunchDuration"
     And the event "app.isLaunching" equals "true"
 
-
+Scenario: Call mark launch complete
+    When I run the game in the "MarkLaunchComplete" state
+    And I wait to receive an error
+    Then the error is valid for the error reporting API sent by the Unity notifier
+    And the exception "message" equals "MarkLaunchComplete"
+    And the event "app.isLaunching" equals "false"
 
---
+++ features/fixtures/maze_runner/Assets/Scripts/Scenarios/Csharp/Config/MarkLaunchComplete.cs
@@ -0,0 +1,21 @@
+﻿using BugsnagUnity;
+
+public class MarkLaunchComplete : Scenario
+{
+    public override void PrepareConfig(string apiKey, string host)
+    {
+        base.PrepareConfig(apiKey, host);
+        Configuration.LaunchDurationMillis = 0;
+    }
+
+    public override void Run()
+    {
+        Invoke("DoException", 10);
+    }
+
+    private void DoException()
+    {
+        Bugsnag.MarkLaunchCompleted();
+        Bugsnag.Notify(new System.Exception("MarkLaunchComplete"));
+    }
+}
