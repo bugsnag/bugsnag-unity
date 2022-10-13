25661b491aba790dbbc175f2608774bfdc1f8a75
rich-bugsnag <83639642+rich-bugsnag@users.noreply.github.com>
2022-10-13 16:35:05 +0200

    task(CI): create metadata scenario classes (#646)

---
+++ features/fixtures/maze_runner/Assets/Scripts/Scenarios/Csharp/Metadata/MetadataAfterStart.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using BugsnagUnity;
+
+public class MetadataAfterStart : Scenario
+{
+    public override void Run()
+    {
+        Bugsnag.AddMetadata("dictionary", new Dictionary<string, object>(){
+            {"foo", "bar" },
+        });
+
+        Bugsnag.AddMetadata("stringArray", "testKey", new string[] { "1", "2", "3" });
+
+        Bugsnag.AddMetadata("numberArray", "testKey", new int[] { 1, 2, 3 });
+
+        Bugsnag.AddMetadata("string", "testKey", "testValue");
+
+        Bugsnag.AddMetadata("string", "testKey2", "testValue2");
+
+        Bugsnag.AddMetadata("number", "testKey", 123);
+
+        Bugsnag.AddMetadata("toClear", "toClear", "toClear");
+
+        Bugsnag.ClearMetadata("toClear");
+
+        Bugsnag.ClearMetadata("string", "testKey");
+
+        DoSimpleNotify("MetadataAfterStart");
+
+    }
+
+  
+}
---
+++ features/fixtures/maze_runner/Assets/Scripts/Scenarios/Csharp/Metadata/MetadataInCallback.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+public class MetadataInCallback : Scenario
+{
+    public override void PrepareConfig(string apiKey, string host)
+    {
+        base.PrepareConfig(apiKey, host);
+        Configuration.AddOnError((@event)=> {
+
+            @event.AddMetadata("dictionary", new Dictionary<string, object>(){
+                {"foo", "bar" },
+            });
+
+            @event.AddMetadata("stringArray", "testKey", new string[] { "1", "2", "3" });
+
+            @event.AddMetadata("numberArray", "testKey", new int[] { 1, 2, 3 });
+
+            @event.AddMetadata("string", "testKey", "testValue");
+
+            @event.AddMetadata("string", "testKey2", "testValue2");
+
+            @event.AddMetadata("number", "testKey", 123);
+
+            @event.AddMetadata("toClear", "toClear", "toClear");
+
+            @event.ClearMetadata("toClear");
+
+            @event.ClearMetadata("string", "testKey");
+
+            return true;
+        });
+    }
+
+    public override void Run()
+    {
+        DoSimpleNotify("MetadataInCallback");
+    }
+}
