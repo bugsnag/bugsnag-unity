d1b1b7b5c4941dc3a21af19d3fb52e29c0e23ae4
rich-bugsnag <83639642+rich-bugsnag@users.noreply.github.com>
2022-10-13 16:30:16 +0200

    task(CI): convert feature flags tests to scenario classes (#645)

---
+++ features/fixtures/maze_runner/Assets/Scripts/Scenarios/Csharp/FeatureFlags/FeatureFlagsInCallback.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeatureFlagsInCallback : Scenario
+{
+    public override void PrepareConfig(string apiKey, string host)
+    {
+        base.PrepareConfig(apiKey, host);
+        Configuration.AddFeatureFlag("testName1", "testVariant1");
+        Configuration.AddFeatureFlag("testName2", "testVariant1");
+        Configuration.AddFeatureFlag("testName2", "testVariant2");
+        Configuration.ClearFeatureFlag("testName1");
+        Configuration.AddOnSendError((@event) => {
+            @event.AddFeatureFlag("testName3", "testVariant3");
+            return true;
+        });
+    }
+
+    public override void Run()
+    {
+        DoSimpleNotify("FeatureFlagsInCallback");
+    }
+}
