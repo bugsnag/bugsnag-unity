686c9e3dcb269aece7f253c5ba6eebe1f6784150
Richard Elms <rich@bugsnag.com>
2022-10-17 12:32:45 +0100

    task(CI): add scenario classes for native macos tests

---
+++ features/fixtures/maze_runner/Assets/Scripts/Scenarios/MacOS/MacOSNativeCrash.cs
@@ -0,0 +1,12 @@
+﻿using BugsnagUnity;
+
+public class MacOSNativeCrash : Scenario
+{
+    public override void Run()
+    {
+        Bugsnag.LeaveBreadcrumb("test");
+        AddTestingMetadata();
+        AddTestingFeatureFlags();
+        MacOSNativeCrash();
+    }
+}
