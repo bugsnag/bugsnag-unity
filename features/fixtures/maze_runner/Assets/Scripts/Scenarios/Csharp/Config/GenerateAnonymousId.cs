8f5b321b61403f09fa9708ca8c577d00b8e5b5d4
Richard Elms <richardelms@gmail.com>
2023-03-14 11:11:14 +0100

    PLAT-9466 make Config.GenerateAnonymousId spec compliant (#704)

---
+++ features/fixtures/maze_runner/Assets/Scripts/Scenarios/Csharp/Config/GenerateAnonymousId.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateAnonymousId : Scenario
+{
+    public override void PrepareConfig(string apiKey, string host)
+    {
+        base.PrepareConfig(apiKey, host);
+        Configuration.GenerateAnonymousId = false;
+    }
+
+    public override void Run()
+    {
+        DoSimpleNotify("GenerateAnonymousId");
+    }
+}
