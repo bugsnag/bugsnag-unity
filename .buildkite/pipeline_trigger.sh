3547cfb4138b64e77c91ec6e1643a37670853416
Josh Edney <josh.edney@smartbear.com>
2024-04-10 16:51:09 +0100

    add 2023 to full pipeline

--- .buildkite/pipeline_trigger.sh
+++ .buildkite/pipeline_trigger.sh
@@ -6,9 +6,9 @@ if [[ "$BUILDKITE_MESSAGE" == *"[full ci]"* ||
   ! -z "$FULL_SCHEDULED_BUILD" ||
   "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "master" ]]; then
   echo "Running full build"
-  buildkite-agent pipeline upload .buildkite/pipeline.full.yml
+  buildkite-agent pipeline upload .buildkite/full/pipeline.full.yml
 else
   # Basic build, but allow a full build to be triggered
   echo "Running basic build"
-  buildkite-agent pipeline upload .buildkite/block.full.yml
+  buildkite-agent pipeline upload .buildkite/full/block.full.yml
 fi
