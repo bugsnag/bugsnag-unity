d03d70f714fe88dff393c9d1da844c3e23e7756c
JamesLindsay0 <James.lindsay@smartbear.com>
2024-07-09 10:37:42 +0100

    Fix timing issues when Bugsnag started after the focus event (#825)

--- src/BugsnagUnity/Client.cs
+++ src/BugsnagUnity/Client.cs
@@ -206,6 +206,8 @@ namespace BugsnagUnity
         {
             _foregroundStopwatch = new Stopwatch();
             _backgroundStopwatch = new Stopwatch();
+            // Required in case the focus event is not recieved (if Bugsnag is started after it is sent)
+            _foregroundStopwatch.Start();
         }
 
         private void InitUserObject()
