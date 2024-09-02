7a532ae8e219597ba7845473754cadd29444f336
Josh Edney <josh.edney@smartbear.com>
2024-09-02 16:03:16 +0100

    update linux build script

--- scripts/ci-run-linux-tests.sh
+++ scripts/ci-run-linux-tests.sh
@@ -23,4 +23,4 @@ pushd features/fixtures/maze_runner/build
 popd
 
 bundle install
-bundle exec maze-runner --app=$APP_PATH --os=linux features/csharp
+bundle exec maze-runner --app=$APP_PATH --os=macos features/csharp
