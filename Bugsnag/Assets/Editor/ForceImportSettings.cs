b6d4a00d03ad5f2e450b014b95103c8b310bc6b2
richard elms <richardelms@gmail.com>
2024-11-04 18:49:02 +0100

    asset plugin importer

--- Bugsnag/Assets/Editor/ForceImportSettings.cs
+++ Bugsnag/Assets/Editor/ForceImportSettings.cs
@@ -1,20 +1,20 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
 [InitializeOnLoad]
 public class ForceImportSettings : MonoBehaviour
 {
-    // Static constructor is called automatically when the project is opened
     static ForceImportSettings()
     {
-        ApplyPluginImportSettings("Assets/Bugsnag/Plugins/iOS", new List<BuildTarget> { BuildTarget.iOS });
-        ApplyPluginImportSettings("Assets/Bugsnag/Plugins/MacOS", new List<BuildTarget> { BuildTarget.StandaloneOSX });
-        ApplyPluginImportSettings("Assets/Bugsnag/Plugins/tvOS", new List<BuildTarget> { BuildTarget.tvOS });
-        ApplyPluginImportSettings("Assets/Bugsnag/Plugins/Android", new List<BuildTarget> { BuildTarget.Android });
-        ApplyPluginImportSettings("Assets/Bugsnag/Plugins/Cocoa", new List<BuildTarget> { BuildTarget.iOS, BuildTarget.tvOS, BuildTarget.StandaloneOSX });
+        EditorApplication.delayCall += () =>
+        {
+            ApplyPluginImportSettings("Assets/Bugsnag/Plugins/iOS", new List<BuildTarget> { BuildTarget.iOS });
+            ApplyPluginImportSettings("Assets/Bugsnag/Plugins/MacOS", new List<BuildTarget> { BuildTarget.StandaloneOSX });
+            ApplyPluginImportSettings("Assets/Bugsnag/Plugins/tvOS", new List<BuildTarget> { BuildTarget.tvOS });
+            ApplyPluginImportSettings("Assets/Bugsnag/Plugins/Android", new List<BuildTarget> { BuildTarget.Android });
+            ApplyPluginImportSettings("Assets/Bugsnag/Plugins/Cocoa", new List<BuildTarget> { BuildTarget.iOS, BuildTarget.tvOS, BuildTarget.StandaloneOSX });
+        };
     }
 
     private static List<BuildTarget> RelevantBuildTargets = new List<BuildTarget> {
@@ -30,29 +30,34 @@ public class ForceImportSettings : MonoBehaviour
 
     private static void ApplyPluginImportSettings(string dir, List<BuildTarget> targets)
     {
-        string[] files = System.IO.Directory.GetFiles(dir, "*", System.IO.SearchOption.AllDirectories);
+        string[] guids = AssetDatabase.FindAssets("", new[] { dir });
 
-        foreach (string file in files)
+        foreach (string guid in guids)
         {
-            // Get the asset importer for each file
-            string assetPath = file.Replace("\\", "/"); // Ensure compatibility on Windows
-            AssetImporter importer = AssetImporter.GetAtPath(assetPath);
+            string assetPath = AssetDatabase.GUIDToAssetPath(guid);
 
-            if (importer != null && !assetPath.EndsWith(".meta"))
+            if (!string.IsNullOrEmpty(assetPath) && !assetPath.EndsWith(".meta"))
             {
-                if (importer is PluginImporter pluginImporter)
-                {
-                    foreach (var target in RelevantBuildTargets)
-                    {
-                        pluginImporter.SetCompatibleWithPlatform(target, targets.Contains(target));
-                    }
-                    pluginImporter.SaveAndReimport();
-                }
+                AssetImporter importer = AssetImporter.GetAtPath(assetPath);
 
-                Debug.Log($"Set import settings for {assetPath}");
+                if (importer != null)
+                {
+                    if (importer is PluginImporter pluginImporter)
+                    {
+                        pluginImporter.SetCompatibleWithAnyPlatform(false);
+                        foreach (var target in RelevantBuildTargets)
+                        {
+                            pluginImporter.SetCompatibleWithPlatform(target, targets.Contains(target));
+                        }
+                        pluginImporter.SaveAndReimport();
+
+                        Debug.Log($"Set import settings for {assetPath}");
+                    }
+                }
             }
         }
 
-        Debug.Log("import settings applied.");
+        AssetDatabase.Refresh();
+        Debug.Log("Import settings applied and Asset Database refreshed.");
     }
 }
