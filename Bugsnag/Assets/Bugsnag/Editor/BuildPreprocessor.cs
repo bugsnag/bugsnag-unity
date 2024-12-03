451283e39dc4bc73757a41285ba4b4bcd9c2ed0c
richard elms <richardelms@gmail.com>
2024-12-03 14:30:39 +0100

    u6 support

--- Bugsnag/Assets/Bugsnag/Editor/BuildPreprocessor.cs
+++ Bugsnag/Assets/Bugsnag/Editor/BuildPreprocessor.cs
@@ -9,21 +9,26 @@ public class BuildPreprocessor : IPreprocessBuildWithReport
 
     public void OnPreprocessBuild(BuildReport report)
     {
-        // This will be enabled once IL2CPP source mapping is supported
-        // if (PlayerSettings.GetScriptingBackend(report.summary.platformGroup) != ScriptingImplementation.IL2CPP)
-        // {
-        //     return;
-        // }
-
-        // // Causes il2cpp to generate my-build-dir/Classes/Native/Symbols/LineNumberMappings.json
-        // // Note: It used to be stored in the project root dir as:
-        // //   my-build-dir_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Symbols/LineNumberMappings.json
-        // const string emitIl2cppSourceMapping = "--emit-source-mapping";
-
-        // var args = PlayerSettings.GetAdditionalIl2CppArgs();
-        // if (!args.Contains(emitIl2cppSourceMapping))
-        // {
-        //     PlayerSettings.SetAdditionalIl2CppArgs(args + emitIl2cppSourceMapping);
-        // }
+        //This will be enabled once IL2CPP source mapping is supported
+        #if UNITY_6000_0_OR_NEWER
+        if (PlayerSettings.GetScriptingBackend(NamedBuildTarget.FromBuildTargetGroup(report.summary.platformGroup)) != ScriptingImplementation.IL2CPP)
+        {
+            return;
+        }
+        #else
+        if (PlayerSettings.GetScriptingBackend(report.summary.platformGroup) != ScriptingImplementation.IL2CPP)
+        {
+            return;
+        }
+        #endif
+        // Causes il2cpp to generate my-build-dir/Classes/Native/Symbols/LineNumberMappings.json
+        // Note: It used to be stored in the project root dir as:
+        //   my-build-dir_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Symbols/LineNumberMappings.json
+        const string emitIl2cppSourceMapping = "--emit-source-mapping";
+        var args = PlayerSettings.GetAdditionalIl2CppArgs();
+        if (!args.Contains(emitIl2cppSourceMapping))
+        {
+            PlayerSettings.SetAdditionalIl2CppArgs(args + emitIl2cppSourceMapping);
+        }
     }
 }
