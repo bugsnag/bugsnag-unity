2f3b6d7c14481e3b45384288395831e4478bd2c7
richard elms <richardelms@gmail.com>
2026-03-26 10:52:21 +0100

    revert maze command change [full ci]

--- features/steps/unity_steps.rb
+++ features/steps/unity_steps.rb
@@ -6,23 +6,8 @@ def execute_command(action, scenario_name = '')
     scenarioName: scenario_name
   }
 
-  enqueued = false
+  Maze::Server.commands.add command
 
-  # Unity fixtures poll `/idem-command` (idempotent command queue).
-  # Older fixtures may poll `/command`, so enqueue to both when available.
-  if Maze::Server.respond_to?(:idem_commands)
-    Maze::Server.idem_commands.add command
-    enqueued = true
-  end
-
-  if Maze::Server.respond_to?(:commands)
-    Maze::Server.commands.add command
-    enqueued = true
-  end
-
-  unless enqueued
-    raise 'No Maze::Server command queue available (expected Maze::Server.commands and/or Maze::Server.idem_commands)'
-  end
 end
 
 Then('the sourcemaps Content-Type header is valid multipart form-data') do
